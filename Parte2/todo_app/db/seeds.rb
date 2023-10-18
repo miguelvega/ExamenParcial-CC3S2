679ef47257324df3ddb25f308567fd429ded0dd5
miguelvega <miguelvegasolde@gmail.com>
2023-10-18 10:06:56 -0500

    Subiendo avance  de la Parte 2

--- Parte2/todo_app/db/seeds.rb
+++ Parte2/todo_app/db/seeds.rb
@@ -6,6 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+
+
 Todo.create(description: "Hello CC3S2- Kids")
 
 Todo.create(description: " Do the assignments kid ")
+
